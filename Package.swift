91679e240778debf2821d343daec5cb035d0d749
khayyamov <khayyamov@users.noreply.github.com>
2025-04-05 09:06:00 +0000

    Libbox Release 1.10.7

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Libbox",
-      url: "https://github.com/khayyamov/xray-singbox-xframework-ios/releases/download/1.12.2/Libbox.xcframework.zip",
-      checksum: "32410fdcb614c3e64d92bb3a6c9e4b0807ac1c0c54748575d4fe806d498498f8"
+      url: "https://github.com/khayyamov/xray-singbox-xframework-ios/releases/download/1.10.7/Libbox.xcframework.zip",
+      checksum: "6da96cba82ade9d09f5bb1bc122d0f0323fbdcc15746bb05aa496cba07d72f5a"
     )
   ]
 )
